519ebd52487a1ad8ee943393b53a5156e68433fb
Hyoung Jo, Bhang <hyoungjo.bhang@gmail.com>
2025-12-20 19:26:02 +0900

    feat(cuda): activate the codes for version 2 kernel optimizations - (2) tensor core matrix multiplication

--- cuda/source/operations.cu
+++ cuda/source/operations.cu
@@ -8,7 +8,7 @@
 
 using namespace nvcuda;
 
-#define TILE_SIZE 16
+#define TILE_SIZE 32
 
 // Ampere Tensor Core Tile Configuration
 #define WMMA_M 16
@@ -642,17 +642,18 @@ void matmul(Tensor &out, const Tensor &A, const Tensor &B) {
   int64_t K = A.shape[1];
   int64_t N = B.shape[1];
 
-  dim3 block_dims(TILE_SIZE, TILE_SIZE);
-  dim3 grid_dims(1 + (N - 1) / TILE_SIZE, 1 + (M - 1) / TILE_SIZE);
+  // dim3 block_dims(TILE_SIZE, TILE_SIZE);
+  // dim3 grid_dims(1 + (N - 1) / TILE_SIZE, 1 + (M - 1) / TILE_SIZE);
   // naive_matrix_multiplication_kernel<<<grid_dims, block_dims>>>(
   //     out.d_data, A.d_data, B.d_data, M, N, K);
-  matrix_multiplication_kernel<<<grid_dims, block_dims>>>(out.d_data, A.d_data,
-                                                          B.d_data, M, N, K);
+  // matrix_multiplication_kernel<<<grid_dims, block_dims>>>(out.d_data,
+  // A.d_data,
+  //                                                         B.d_data, M, N, K);
 
-  // dim3 block_dims(128); // 32 x 4 warps
-  // dim3 grid_dims(1 + (N - 1) / TILE_SIZE, 1 + (M - 1) / TILE_SIZE);
-  // tensor_core_matrix_multiplication_kernel<<<grid_dims, block_dims>>>(
-  //     out.d_data, A.d_data, B.d_data, M, N, K);
+  dim3 block_dims(128); // 32 x 4 warps
+  dim3 grid_dims(1 + (N - 1) / TILE_SIZE, 1 + (M - 1) / TILE_SIZE);
+  tensor_core_matrix_multiplication_kernel<<<grid_dims, block_dims>>>(
+      out.d_data, A.d_data, B.d_data, M, N, K);
 }
 
 void add(Tensor &x, const Tensor &y) {
