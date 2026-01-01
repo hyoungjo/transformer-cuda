b2b989fae52755ef72fa24a77d5fc9e4b6b93ed2
Hyoung Jo, Bhang <hyoungjo.bhang@gmail.com>
2026-01-01 15:50:10 +0900

    feat(llama): activate the codes for version 2 kernel optimizations - (1) matrix multiplication

--- cuda/source/operations.cu
+++ cuda/source/operations.cu
@@ -860,10 +860,10 @@ void matmul(Tensor &out, const Tensor &A, const Tensor &B, bool transpose_b) {
 
   dim3 block_dims(TILE_SIZE, TILE_SIZE);
   dim3 grid_dims(1 + (N - 1) / TILE_SIZE, 1 + (M - 1) / TILE_SIZE);
-  naive_matrix_multiplication_kernel<<<grid_dims, block_dims>>>(
-      out.d_data, A.d_data, B.d_data, M, N, K, transpose_b);
-  // matrix_multiplication_kernel<<<grid_dims, block_dims>>>(
+  // naive_matrix_multiplication_kernel<<<grid_dims, block_dims>>>(
   //     out.d_data, A.d_data, B.d_data, M, N, K, transpose_b);
+  matrix_multiplication_kernel<<<grid_dims, block_dims>>>(
+      out.d_data, A.d_data, B.d_data, M, N, K, transpose_b);
 
   // dim3 block_dims(128); // 32 x 4 warps
   // dim3 grid_dims(1 + (N - 1) / TILE_SIZE, 1 + (M - 1) / TILE_SIZE);
